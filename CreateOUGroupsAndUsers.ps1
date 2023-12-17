37cc102583697ca4d1eacc92493e10d00d5cc72f
Aleksandr Kustov <41970276+alexust1820@users.noreply.github.com>
2023-12-17 19:46:32 +0300

    Add files via upload

--- CreateOUGroupsAndUsers.ps1
+++ CreateOUGroupsAndUsers.ps1
@@ -74,6 +74,7 @@ $ADGroupsArray = @(
 #Создание групп пользователей
 Write-Output('Создание групп пользователей:')
 Foreach($group in $ADGroupsArray) {
+    #Проверка доменной структуры на наличие (или отсутствие) группы
     if(!$(Get-ADGroup -Filter "Name -like '$($group.gName)'")) {
         New-ADGroup -Name $($group.gName) `
             -DisplayName $($group.gName) `
@@ -82,6 +83,8 @@ Foreach($group in $ADGroupsArray) {
             -Description $($group.desc) `
             -Path 'OU=KUSTORG,DC=KUSTOV,DC=LOCAL' `
             -ManagedBy 'Администратор'
+        
+        #Добавление только что созданной группы в группу "Пользователи домена"
         Add-ADGroupMember -Identity "Пользователи домена" -Members $($group.gName)
         Write-Output("    Группа $($group.gName) создана.")
 
@@ -93,3 +96,33 @@ Write-Output('=========================================')
 #=======Создание пользователей========
 
 Write-Output('Создание пользователей:')
+
+#Импорт пользователей из файла
+$ADUsers = Import-Csv -Path 'C:\Users\Администратор\Documents\users.csv'
+
+#Создание пользователей в домене
+Foreach($user in $ADUsers) {
+    #Проверка существует ли пользователь или нет
+    if(!$(Get-ADUser -Filter "Name -like '$($user.username)'")) {
+        
+        #Создание пользователей в домене
+        New-ADUser -Name $($user.username) `
+            -Surname $($user.surname) `
+            -GivenName $($user.name) `
+            -AccountPassword $(
+                ConvertTo-SecureString `
+                -String $($user.password) `
+                -AsPlainText `
+                -Force
+             ) `
+            -Path "OU=Users,OU=$($user.office),OU=$($OURootName),$($x500Domain)" `
+            -Enabled $True
+
+        #Добавление в соответствущую группу
+        Add-ADGroupMember -Identity $($user.group) -Members $($user.username)
+
+        Write-Output("    Пользователь $($user.username) создан и добавлен в группу $($user.group).")
+    } else {
+        Write-Output("    Пользователь $($user.username) существует!")
+    }
+}
